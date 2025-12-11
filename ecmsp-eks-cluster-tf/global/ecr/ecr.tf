dcc4bf5aba5c1598b936fcf9323ba84a3f3b4a7e
pawlowiczf <pawlowicz.filip137@gmail.com>
2025-12-11 16:31:16 +0100

    change lifecycle policy

--- ecmsp-eks-cluster-tf/global/ecr/ecr.tf
+++ ecmsp-eks-cluster-tf/global/ecr/ecr.tf
@@ -10,12 +10,12 @@ module "ecr" {
     rules = [
       {
         rulePriority = 1,
-        description  = "Keep last 30 images",
+        description  = "Keep last 3 images",
         selection = {
           tagStatus = "tagged",
           tagPatternList : ["v"]
           countType   = "imageCountMoreThan",
-          countNumber = 30
+          countNumber = 3
         },
         action = {
           type = "expire"
